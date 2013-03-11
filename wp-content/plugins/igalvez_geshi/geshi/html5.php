24a848ee22ff09387bfcb3fc7237537f9ad4b134
Israel Galvez <i@igalvez.net>
2013-03-11 02:48:41 -0700

    change language name to "HTML"

--- wp-content/plugins/igalvez_geshi/geshi/html5.php
+++ wp-content/plugins/igalvez_geshi/geshi/html5.php
@@ -50,7 +50,7 @@
  ************************************************************************************/
 
 $language_data = array (
-    'LANG_NAME' => 'HTML5',
+    'LANG_NAME' => 'HTML',
     'COMMENT_SINGLE' => array(),
     'COMMENT_MULTI' => array(),
     'CASE_KEYWORDS' => GESHI_CAPS_NO_CHANGE,
