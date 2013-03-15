53a9ac1b037bd93863d733b47c63381d4706e83a
Israel Galvez <i@igalvez.net>
2013-03-15 16:29:49 -0700

    Solarized and Tango PHP palettes for use with CSS files

---
+++ wp-content/themes/igalvez-net/css/palettes.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+"Solarized" palette by Ethan Schoonover.
+http://ethanschoonover.com/solarized
+*/
+$solarized_base03  = "#002b36";
+$solarized_base02  = "#073642";
+$solarized_base01  = "#586e75";
+$solarized_base00  = "#657b83";
+$solarized_base0   = "#839496";
+$solarized_base1   = "#93a1a1";
+$solarized_base2   = "#eee8d5";
+$solarized_base3   = "#fdf6e3";
+$solarized_yellow  = "#b58900";
+$solarized_orange  = "#cb4b16";
+$solarized_red     = "#dc322f";
+$solarized_magenta = "#d33682";
+$solarized_violet  = "#6c71c4";
+$solarized_blue    = "#268bd2";
+$solarized_cyan    = "#2aa198";
+$solarized_green   = "#859900";
+
+/*
+"Tango" palette.
+http://tango.freedesktop.org/
+*/
+$tango_butter1     = "#fce94f";
+$tango_butter2     = "#edd400";
+$tango_butter3     = "#c4a000";
+$tango_orange1     = "#fcaf3e";
+$tango_orange2     = "#f57900";
+$tango_orange3     = "#ce5c00";
+$tango_chocolate1  = "#e9b96e";
+$tango_chocolate2  = "#c17d11";
+$tango_chocolate3  = "#8f5902";
+$tango_chameleon1  = "#8ae234";
+$tango_chameleon2  = "#73d216";
+$tango_chameleon3  = "#4e9a06";
+$tango_skyblue1    = "#729fcf";
+$tango_skyblue2    = "#3465a4";
+$tango_skyblue3    = "#204a87";
+$tango_plum1       = "#ad7fa8";
+$tango_plum2       = "#75507b";
+$tango_plum3       = "#5c3566";
+$tango_scarletred1 = "#ef2929";
+$tango_scarletred2 = "#cc0000";
+$tango_scarletred3 = "#a40000";
+$tango_aluminium1  = "#eeeeec";
+$tango_aluminium2  = "#d3d7cf";
+$tango_aluminium3  = "#babdb6";
+$tango_aluminium4  = "#888a85";
+$tango_aluminium5  = "#555753";
+$tango_aluminium6  = "#2e3436";
+
+?>
