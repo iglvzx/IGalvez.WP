69c62e5cd0787ae09477ffc52c0103b8113885fa
Israel Galvez <i@igalvez.net>
2013-03-23 19:38:01 -0700

    make the Google CSE logo a link

--- wp-content/themes/igalvez-net/search-form.php
+++ wp-content/themes/igalvez-net/search-form.php
@@ -1,6 +1,11 @@
 <form id="search-form" method="GET" action="<?php bloginfo('wpurl'); ?>/search/">
     <p id="search-box">
-        <label for="query">Query</label><span id="google-branding"><img id="google-logo" src="http://www.google.com/uds/css/small-logo.png"/>Custom Search</span>
+        <label for="query">Query</label>
+        <span id="google-branding">
+            <a href="http://google.com/cse/">
+                <img id="google-logo" src="http://www.google.com/uds/css/small-logo.png"/>Custom Search
+            </a>
+        </span>
         <input id="query" name="q" type="text" value="<?php echo $query; ?>"/>
     </p>
     <p>
